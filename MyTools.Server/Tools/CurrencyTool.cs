caacc6dff633f757687f0af8e7fdecf75871c90b
Ali Farahnak <alfarahn@microsoft.com>
2025-04-07 21:20:40 +0200

    m

--- MyTools.Server/Tools/CurrencyTool.cs
+++ MyTools.Server/Tools/CurrencyTool.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace MyTools.Server;
+namespace MyTools.Server.Tools;
 
 [McpServerToolType]
 public static class CurrencyTool
--- MyTools.Server/Tools/WeatherTool.cs
+++ MyTools.Server/Tools/WeatherTool.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Net.Http.Json;
 using System.Text.Json;
 
-namespace MyTools.Server;
+namespace MyTools.Server.Tools;
 
 [McpServerToolType]
 public static class WeatherTool
